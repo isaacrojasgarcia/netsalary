a759888e573edc2962278cec62e496cc203be273
Lt <trepafi@gmail.com>
2014-11-20 23:49:56 +0100

    Adding chart

--- client/lib/content.js
+++ client/lib/content.js
@@ -1,12 +1,11 @@
 if (Meteor.isClient) {
 	var mySalary = new MySalary();
 	var inputGross = null;
-	var result = {
-		gross: 0,
-		net: 0
-	};
 
+	var gross = 0, result = {};
 
+	var pieChart, chartContext;
+	
 	// counter starts at 0
 	Session.setDefault("counter", 0);
 
@@ -18,18 +17,52 @@ if (Meteor.isClient) {
 
 	Template.content.events({
 		'keyup .calculate-input': function (event) {
-			result.gross = inputGross.val();
-			mySalary.setSalary(parseInt(result.gross));
-			result.net = mySalary.calculateNetSalary();
+			gross = inputGross.val();
+			mySalary.setSalary(parseInt(gross));
+			result = mySalary.calculateSalary();
 
-			$('#gross-salary').html(getFormatedSalary(result.gross));
+			$('#gross-salary').html(getFormatedSalary(gross));
 			$('#net-salary').html(getFormatedSalary(result.net));
 			$('#monthly-salary').html(getFormatedSalary(result.net / 12));
+
+			pieChart.segments[0].value = Math.round(result.net / gross * 100);
+			pieChart.segments[1].value = Math.round(result.irpf / gross * 100);
+			pieChart.segments[2].value = Math.round(result.ss / gross * 100);
+			pieChart.update();
 		}
 	});
 
 	Template.content.rendered = function() {
 		inputGross = $('.calculate-input').focus();
+
+		var data = [
+			    {
+			        value: 100,
+			        color:"#F7464A",
+			        highlight: "#FF5A5E",
+			        label: "Net Salary"
+			    },
+			    {
+			        value: 0,
+			        color: "#46BFBD",
+			        highlight: "#5AD3D1",
+			        label: "IRPF"
+			    },
+			    {
+			        value: 0,
+			        color: "#FDB45C",
+			        highlight: "#FFC870",
+			        label: "Social Security"
+			    }
+			];
+
+		var options = {
+		    responsive : true
+		};
+
+		// Chart
+		chartContext = $('#result-chart').get(0).getContext("2d");
+		pieChart = new Chart(chartContext).Doughnut(data, options);
 	};
 
 	function getFormatedSalary(salary) {
